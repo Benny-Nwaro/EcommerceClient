82b3a6e724d6918a19590cb002acef2b13d2b448
AROH EBENEZER <baroh3767@aol.com>
2025-06-18 14:32:53 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,7 @@ import {jwtDecode} from "jwt-decode";
 const isDevelopment = window.location.hostname.includes("localhost");
 
 const getServer = () =>{
-   return isDevelopment ? "https://eshop-439007.uc.r.appspot.com" : "https://eshop-439007.uc.r.appspot.com";
+   return isDevelopment ? "https://ecommerceserver-fxfb.onrender.com" : "https://ecommerceserver-fxfb.onrender.com";
 }
 const decodeUser = () => {
    const token = localStorage.getItem("token");
